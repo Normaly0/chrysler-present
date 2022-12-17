6ab029840ce10e57d99cfb6e1558c17152b02cbd
Normaly <40768391+Normaly0@users.noreply.github.com>
2022-12-17 22:06:59 +0100

    new public path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   base: 'https://normaly.net/dist',
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()]
+    }
+  } else {
+    return {
+      base: 'https://normaly.net/dist',
+      plugins: [react()]
+    }
+  }
 })
